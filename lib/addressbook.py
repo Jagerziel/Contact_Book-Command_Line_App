efae67f2fc28c0ad79e5a58a3610b1697b75da2c
Jagerziel <fromnytosg@gmail.com>
2023-02-04 16:44:05 -0500

    Add conditional options

--- lib/addressbook.py
+++ lib/addressbook.py
@@ -4,10 +4,23 @@
 # Start the app
 while True:
     print('Select an option: ')
-    print('a) See Address Book\nb) Add an Entry\nc) Update an Entry\nd) Delete an Entry\n')
+    print('a) See Address Book\nb) Add an Entry\nc) Update an Entry\nd) Delete an Entry')
     opt = input('Your selection: ').lower()
     while opt != 'a' and opt != 'b' and opt != 'c' and opt != 'd':
         print('Please make a valid selection.')
-        print('a) See Address Book\nb) Add an Entry\nc) Update an Entry\nd) Delete an Entry\n')
+        print('a) See Address Book\nb) Add an Entry\nc) Update an Entry\nd) Delete an Entry')
         opt = input('Your selection: ').lower()
+    # See Address Book
+    if opt == 'a':
+        print("See Address Book")
+    # Add Entry to Address Book
+    if opt == 'b':
+        print("Add Entry")
+    # Edit Entry in Address Book
+    if opt == 'c':
+        print("Edit Entry")
+    # Delete Entry in Address Book
+    if opt == 'd':
+        print('Delete Entry')
+
     break;
